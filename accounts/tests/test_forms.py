5131992f604635358cd6da15ae8d408e866f11c8
josep-pujol <pujol.rabat@gmail.com>
2020-01-19 21:03:58 +0000

    setup test database as no permissions to setup another database in elephantsql. created form tests

--- accounts/tests/test_forms.py
+++ accounts/tests/test_forms.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import User
 from django.test import TestCase
-from django.urls import reverse
 
 
 class UserRegistrationFormTest(TestCase):
@@ -8,25 +7,37 @@ class UserRegistrationFormTest(TestCase):
     def setUp(self):
         print('\n\nsetup')
         user2test = User.objects.create_user(
-            username='user2test', email='user2_test@email.com',
-            password1='1X<ISRUkw+tuK', password2='1X<ISRUkw+tuK',
+            username='user2test', email='usertest@email.com',
+            password='XISRUkwtuK',
         )
         user2test.save()
-        print(user2test)
 
-    def test_email_address_already_registered(self):
-        # Raise validation error when Email address is already
-        # registered
-        print('\n\ntest')
+    def test_username__already_registered(self):
+        # Raise validation error when trying to register a username
+        # that is already in use
         response = self.client.post(
-            reverse('/register/'),
+            '/accounts/register/',
             {
-             'username': 'user', 'email': 'user2_test@email.com',
-             'password1': '1X<ISRUkw+tuK', 'password2': '1X<ISRUkw+tuK',
+             'username': 'user', 'email': 'usertest@email.com',
+             'password': 'XISRUkwtuK',
              }
         )
-        print(response)
         self.assertFormError(
             response, 'registration_form', 'email',
             'This email address is already in use'
-        ) 
+        )
+
+    def test_email_address_already_registered(self):
+        # Raise validation error when trying to register an email
+        # address that is already in use
+        response = self.client.post(
+            '/accounts/register/',
+            {
+             'username': 'user2test', 'email': 'user2test@email.com',
+             'password': 'XISRUkwtuK',
+             }
+        )
+        self.assertFormError(
+            response, 'registration_form', 'username',
+            'A user with that username already exists.'
+        )
