7197f9e5456f2d7d76429a3876d89b0e80cd034f
josep-pujol <pujol.rabat@gmail.com>
2020-02-07 18:13:14 +0000

    updated apps.py to reset the database sequence of the primary keys or ids. Needed because we add default values in migrations

--- tasks/apps.py
+++ tasks/apps.py
@@ -1,5 +1,34 @@
+import re
+from io import StringIO
+
 from django.apps import AppConfig
+from django.core.management import call_command
+from django.db import connection
+from django.db.models.signals import post_migrate
+
+
+def reset_sequences(sender, **kwargs):
+    """ reset the sql sequences """
+    print('reset_sequences')
+
+    # list of apps to reset 
+    app_names = ['tasks', ]
+
+    for app_name in app_names:
+        output = StringIO()
+        call_command('sqlsequencereset', app_name, stdout=output)
+        sql = output.getvalue()
+
+        # Remove terminal color codes from sqlsequencereset output
+        ansi_escape = re.compile(r'\x1b[^m]*m')
+        sql = ansi_escape.sub('', sql)
+
+        with connection.cursor() as cursor:
+            cursor.execute(sql)
 
 
 class TasksConfig(AppConfig):
     name = 'tasks'
+
+    def ready(self):
+        post_migrate.connect(reset_sequences, sender=self)
