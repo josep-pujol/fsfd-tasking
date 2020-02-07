8b45f2851cb2506c0c58b97b0a919f784767d8a5
josep-pujol <pujol.rabat@gmail.com>
2020-02-07 08:25:54 +0000

    registered tasks model to admin

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from tasks.models import Category, Group, Importance, Status, Task, UserGroup
+
+admin.site.register(Category)
+admin.site.register(Importance)
+admin.site.register(Group)
+admin.site.register(Status)
+admin.site.register(Task)
+admin.site.register(UserGroup)
