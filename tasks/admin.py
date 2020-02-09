2251934649c8313cf5d311f7286ca97c48ee790e
josep-pujol <pujol.rabat@gmail.com>
2020-02-09 01:39:22 +0000

    added filters in admin panel for UserTeam

--- tasks/admin.py
+++ tasks/admin.py
@@ -2,9 +2,15 @@ from django.contrib import admin
 
 from tasks.models import Category, Importance, Status, Task, Team, UserTeam
 
+
 admin.site.register(Category)
 admin.site.register(Importance)
 admin.site.register(Team)
 admin.site.register(Status)
 admin.site.register(Task)
-admin.site.register(UserTeam)
+
+
+@admin.register(UserTeam)
+class UserTeamAdmin(admin.ModelAdmin):
+    list_display = ('ut_user', 'ut_team', )
+    list_filter = ('ut_user', 'ut_team', )
