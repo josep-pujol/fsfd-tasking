c797eb3751201241224b430d0faebc919f4a3ccd
josep-pujol <pujol.rabat@gmail.com>
2020-02-12 01:22:57 +0000

    added completed tasks page and linked templates  with views urls etc.

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
-from tasks.views import (assigned_tasks, create_task, team_tasks,
-                         user_tasks, update_status, update_task)
+from tasks.views import (
+    assigned_tasks, completed_tasks, create_task,
+    team_tasks, user_tasks, update_status, update_task
+)
 
 
 urlpatterns = [
     path('', user_tasks, name='user_tasks'),
     path('assigned-tasks/', assigned_tasks, name='assigned_tasks'),
+    path('completed-tasks/', completed_tasks, name='completed_tasks'),
     path('team-tasks/', team_tasks, name='team_tasks'),
     path('create/', create_task, name='create_task'),
     path('update/task/<int:pk>/', update_task, name='update_task'),
--- tasks/views.py
+++ tasks/views.py
@@ -1,11 +1,11 @@
+import datetime
+
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, redirect, render
-from django.urls import reverse, reverse_lazy
+from django.urls import reverse
 
-from subscriptions.models import PremiumUser
 from tasks.forms import EditStatusForm, TasksForm
 from tasks.models import Category, Importance, Status, Task, Team, UserTeam
 
@@ -18,7 +18,11 @@ def get_users_in_team(team):
 
 @login_required
 def user_tasks(request):
-    tasks = Task.objects.filter(tsk_user_id=request.user.pk, tsk_team_id=1)
+    tasks = Task.objects.filter(
+        tsk_user_id=request.user.pk,
+        tsk_team_id=1,
+        finishdate__isnull=True
+    )
     status = Status.objects.all()
     context = {
         'section_title': 'Your Tasks',
@@ -38,7 +42,9 @@ def assigned_tasks(request):
     else:
         ids_to_exclude = [1, ]
     tasks = Task.objects.filter(
-        tsk_user_id=request.user.pk).exclude(tsk_team_id__in=ids_to_exclude)
+        tsk_user_id=request.user.pk,
+        finishdate__isnull=True
+        ).exclude(tsk_team_id__in=ids_to_exclude)
     status = Status.objects.all()
     context = {
         'section_title': 'Assigned Tasks',
@@ -49,7 +55,6 @@ def assigned_tasks(request):
     return render(request, 'tasks/tasks_table.html', context=context)
 
 
-# TODO
 @login_required
 def team_tasks(request):
     # Only for premium users
@@ -57,7 +62,8 @@ def team_tasks(request):
     is_team_owner = hasattr(user, 'team_owner')
     if is_team_owner:
         team_id = request.user.team_owner.pk
-        tasks = Task.objects.filter(tsk_team_id=team_id)
+        tasks = Task.objects.filter(
+            tsk_team_id=team_id, finishdate__isnull=True)
         status = Status.objects.all()
         context = {
             'section_title': 'Your Team Task List',
@@ -65,10 +71,22 @@ def team_tasks(request):
             'tasks': tasks,
             'status': status,
         }
-        return render(request, 'tasks/tasks_table.html', context=context)
+        return render(
+            request, 'tasks/completed_tasks_table.html', context=context)
     else:
         messages.info(request, 'Only Team Owners can access a Team Task List.')
         return render(redirect('index'))
+
+
+@login_required
+def completed_tasks(request):
+    tasks = Task.objects.filter(
+        tsk_user_id=request.user.pk, finishdate__isnull=False)
+    context = {
+        'section_title': 'Completed Tasks',
+        'tasks': tasks,
+    }
+    return render(request, 'tasks/tasks_table.html', context=context)
 
 
 @login_required
@@ -77,6 +95,7 @@ def create_task(request):
     is_team_owner = hasattr(user, 'team_owner')
     if request.method == 'POST':
         task_form = TasksForm(request.POST)
+        print(vars(task_form))
         if task_form.is_valid():
             task = Task()
             task.tsk_name = task_form.cleaned_data['tsk_name']
@@ -86,6 +105,13 @@ def create_task(request):
             task.tsk_category = task_form.cleaned_data['tsk_category']
             task.tsk_importance = task_form.cleaned_data['tsk_importance']
             task.tsk_status = task_form.cleaned_data['tsk_status']
+            print(vars(task))
+            # If user set task as completed then startdate and
+            # finished date are to be set at todays date
+            if task.tsk_status.sta_name == 'Completed':
+                today_ = datetime.datetime.today().date()
+                task.startdate = today_
+                task.finishdate = today_
             # Select task team in function of user assigned to task
             if int(task.tsk_user.pk) == int(user.pk):
                 task.tsk_team = Team.objects.get(pk=1)  # Default Team
@@ -120,6 +146,7 @@ def update_task(request, pk):
     if request.method == 'POST':
         task = get_object_or_404(Task, pk=pk)
         task_form = TasksForm(request.POST)
+        print(vars(task_form))
         if task_form.is_valid():
             task.tsk_name = task_form.cleaned_data['tsk_name']
             task.tsk_user = task_form.cleaned_data['tsk_user']
@@ -128,6 +155,14 @@ def update_task(request, pk):
             task.tsk_category = task_form.cleaned_data['tsk_category']
             task.tsk_importance = task_form.cleaned_data['tsk_importance']
             task.tsk_status = task_form.cleaned_data['tsk_status']
+            print(vars(task))
+            # If user set task as completed then startdate and
+            # finished date are to be set at todays date
+            if task.tsk_status.sta_name == 'Completed':
+                today_ = datetime.datetime.today().date()
+                task.startdate = today_
+                task.finishdate = today_
+                print(vars(task))
             # Select task team in function of user assigned to task
             if int(task.tsk_user.pk) == int(user.pk):
                 task.tsk_team = Team.objects.get(pk=1)  # Default Team
