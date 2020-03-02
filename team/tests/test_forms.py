058902bc5404ecf75fede11f0706f7ddc68bc683
josep-pujol <pujol.rabat@gmail.com>
2020-03-02 23:27:20 +0000

    refractored. Moved login user to setup method instead of being in each test

--- team/tests/test_forms.py
+++ team/tests/test_forms.py
@@ -6,13 +6,13 @@ from team.forms import AddCollaboratorForm
 
 class AddCollaboratorFormTest(TestCase):
 
-    def test_invalid_emails_return_invalid_form(self):
+    def test_invalid_email_return_invalid_form(self):
         invalid_emails = ['email.invalid.com', 'aa', '@adf.com', ]
         for email in invalid_emails:
             form = AddCollaboratorForm(data={'email_to_add': email})
             self.assertFalse(form.is_valid())
 
-    def test_valid_unregistered_email_return_invalid_form(self):
+    def test_valid_unregistered_email_returns_invalid_form(self):
         valid_unregistered_email = 'user_unregistered_test@email.com'
         form = AddCollaboratorForm(
             data={'email_to_add': valid_unregistered_email})
--- team/tests/test_views.py
+++ team/tests/test_views.py
@@ -9,7 +9,7 @@ from team.models import Team, UserTeam
 class UserTeamViewTest(TestCase):
 
     @classmethod
-    def setUp(self):
+    def setUpTestData(cls):
         # Create test users
         user2test = User.objects.create_user(
             username='user2test', email='usertest@email.com',
@@ -39,90 +39,61 @@ class UserTeamViewTest(TestCase):
         )
         user_team.save()
 
+    def setUp(self):
+        # Login user
+        self.client.login(username='user2test', password='XISRUkwtuK')
+
     def test_collaborators_url_exists(self):
-        login = self.client.login(
-            username='user2test', password='XISRUkwtuK')
-        self.assertTrue(login)
         response = self.client.get('/team/collaborators/')
         self.assertEqual(response.status_code, 200)
 
     def test_add_collaborators_url_exists(self):
-        login = self.client.login(
-            username='user2test', password='XISRUkwtuK')
-        self.assertTrue(login)
         response = self.client.get('/team/collaborators/add/')
         self.assertEqual(response.status_code, 200)
 
     def test_collaborators_url_accessible_by_name(self):
-        login = self.client.login(
-            username='user2test', password='XISRUkwtuK')
-        self.assertTrue(login)
         response = self.client.get(reverse('team_collaborators'))
         self.assertEqual(response.status_code, 200)
 
     def test_add_collaborators_url_accessible_by_name(self):
-        login = self.client.login(
-            username='user2test', password='XISRUkwtuK')
-        self.assertTrue(login)
         response = self.client.get(reverse('add_collaborator'))
         self.assertEqual(response.status_code, 200)
 
     def test_view_uses_correct_template(self):
-        login = self.client.login(
-            username='user2test', password='XISRUkwtuK')
-        self.assertTrue(login)
-        # Team Collaborator
-        response = self.client.get(reverse('team_collaborators'))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'team/team_collaborators.html')
-        # Add Collaborator
-        response = self.client.get(reverse('add_collaborator'))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'team/team_collaborators.html')
-
-    def test_user_redirected_when_not_logged_or_unknown_user(self):
-        # Trying to Login unkwnow user
-        login = self.client.login(
-            username='testuser_unknown', password='XISRUkwtuK')
-        self.assertFalse(login)
-        response = self.client.get(reverse('team_collaborators'), follow=True)
-        self.assertEqual(response.status_code, 200)
-        self.assertRedirects(response,
-                             '/accounts/login/?next=/team/collaborators/')
+        # Team Collaborators url
+        response_team = self.client.get(reverse('team_collaborators'))
+        # Add Collaborator url
+        response_add = self.client.get(reverse('add_collaborator'))
+        self.assertEqual(response_team.status_code, 200)
+        self.assertTemplateUsed(response_team, 'team/team_collaborators.html')
+        self.assertEqual(response_add.status_code, 200)
+        self.assertTemplateUsed(response_add, 'team/team_collaborators.html')
 
     def test_premium_user_can_add_registered_user_to_team(self):
-        login = self.client.login(
-            username='user2test', password='XISRUkwtuK')
-        self.assertTrue(login)
-        response = self.client.get(reverse('add_collaborator'))
-        self.assertEqual(response.status_code, 200)
-
         response = self.client.post(reverse('add_collaborator'),
                                     {'email_to_add': 'usertest_B@email.com'},
                                     follow=True)
-        self.assertEqual(response.status_code, 200)
         msgs = [msg.__str__() for msg in get_messages(response.wsgi_request)]
-        self.assertIn('Added to your Team!', msgs)
         premium_user = User.objects.get(email='usertest@email.com')
         user_added2team = User.objects.get(email='usertest_B@email.com')
         user_team = UserTeam.objects.get(
             ut_team=premium_user.team_owner.pk, ut_user=user_added2team.pk)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('Added to your Team!', msgs)
         self.assertEqual('usertest_B@email.com', user_team.ut_user.email)
 
     def test_premium_user_get_error_msg_if_adding_existing_team_user(self):
-        login = self.client.login(
-            username='user2test', password='XISRUkwtuK')
-        self.assertTrue(login)
-        # Add user into team
+        # Add Team user
         response = self.client.post(reverse('add_collaborator'),
                                     {'email_to_add': 'usertest_B@email.com'},
                                     follow=True)
-        self.assertEqual(response.status_code, 200)
         premium_user = User.objects.get(email='usertest@email.com')
         user_added2team = User.objects.get(email='usertest_B@email.com')
         user_team = UserTeam.objects.get(
             ut_team=premium_user.team_owner.pk, ut_user=user_added2team.pk)
+        self.assertEqual(response.status_code, 200)
         self.assertEqual('usertest_B@email.com', user_team.ut_user.email)
+
         # Try a second time to get error
         response = self.client.post(reverse('add_collaborator'),
                                     {'email_to_add': 'usertest_B@email.com'},
@@ -133,16 +104,24 @@ class UserTeamViewTest(TestCase):
             'usertest_B@email.com is already a Collaborator in the Team', msgs)
 
     def test_premium_user_gets_error_when_adding_unregistered_user(self):
-        login = self.client.login(
-            username='user2test', password='XISRUkwtuK')
-        self.assertTrue(login)
-        # Add user into team
         response = self.client.post(
             reverse('add_collaborator'),
             {'email_to_add': 'not_registered@email.com'},
             follow=True
         )
-        self.assertEqual(response.status_code, 200)
         msgs = [msg.__str__() for msg in get_messages(response.wsgi_request)]
+        self.assertEqual(response.status_code, 200)
         self.assertIn(
             'Please ensure the email belongs to a Tasking user', msgs)
+
+    def test_user_redirected_when_not_logged_or_unknown_user(self):
+        # Clear session logging out user
+        self.client.logout()
+        # Trying to Login unkwnow user
+        login = self.client.login(
+            username='testuser_unknown', password='XISRUkwtuK')
+        response = self.client.get(reverse('team_collaborators'), follow=True)
+        self.assertFalse(login)
+        self.assertEqual(response.status_code, 200)
+        self.assertRedirects(response,
+                             '/accounts/login/?next=/team/collaborators/')
